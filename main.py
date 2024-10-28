e47a1fd6b97701899bda34ebc2faca81cc53531f
CoNickel-MC <springterror228@gmail.com>
2024-10-28 14:44:32 +0530

    Time reduced to 0.5 seconds

--- main.py
+++ main.py
@@ -136,6 +136,6 @@ def checkListenTime():
             except Exception as e:
                 logger.error(f"Error processing user {userFromCollection.get('emailId')}: {e}")
 
-        time.sleep(10)
+        time.sleep(0.5)
 
 threading.Thread(target=checkListenTime, daemon=True).start()
